47f14e059736114ea0bf247b3900cac7921854e2
chaikou oughailou balde <chaikoubalde0@gmail.com>
2022-10-23 18:17:25 +0000

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionStockproduitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockproduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockproduitsApplication.class, args);
+	}
+
+}
